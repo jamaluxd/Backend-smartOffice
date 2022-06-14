fe63024d40df38c00655c20d64e8816b8620f6b7
Jamal <91877192+jamaluxd@users.noreply.github.com>
2022-06-14 11:07:43 +0600

    first

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // initailization
+require('dotenv').config();
+
+
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose.connect(process.env.Mongo_URL, {
+    useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log("Conntected to Database"));
+
+app.use(express.json());
+
+// const subscribersRouter = require('./routes/subscribers');
+
+// app.use('/subscribers', subscribersRouter);
+
+app.listen(process.env.PORT, () => console.log("Server Started"));
