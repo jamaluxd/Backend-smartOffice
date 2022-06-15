e57499f041c6ad587f33592a17b14b5f3383d101
Jamal <91877192+jamaluxd@users.noreply.github.com>
2022-06-15 16:58:00 +0600

    final 6/15

--- app.js
+++ app.js
@@ -22,9 +22,12 @@ db.once("open", () => console.log("Conntected to Database"));
 
 
 const showAllemployeesRouter = require("./routes/employee/user/show_all_employees");
-const createNewemployeeRouter= require("./routes/employee/user/create_new_emplyee");
+const createNewemployeeRouter= require("./routes/employee/admin/create_new_emplyee");
+// const userLoginRouter= require("./routes/employee/user/user_login");
 
 app.use("/routes/employee/user/showAllEmployees", showAllemployeesRouter);
-app.use("/routes/employee/user/createNewEmployee", createNewemployeeRouter);
+
+app.use("/routes/employee/admin/createNewEmployee", createNewemployeeRouter);
+// app.use("/routes/employee/user/userLogin", userLoginRouter);
 
 app.listen(process.env.PORT, () => console.log("Server Started"));
