50439a93c96aa9ef121049ba74afeeff93c392b0
Jamal <91877192+jamaluxd@users.noreply.github.com>
2022-06-21 16:38:03 +0600

    final

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -1,10 +1,9 @@
 const jwt = require("jsonwebtoken");
-const checkLogin = (req, res, next) => {
+const checkLogin = async (req, res, next) => {
   const { authorization } = req.headers;
-
   try {
     const token = authorization.split(" ")[1];
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    const decode = await jwt.verify(token, process.env.JWT_SECRET);
     const { id, admin } = decode;
     req.id = id;
     req.admin = admin;
