eff2318d77b883b67d00bcd485186132d1d36841
Jamal <91877192+jamaluxd@users.noreply.github.com>
2022-06-24 16:53:36 +0600

    final

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -4,23 +4,33 @@ const checkLogin = async (req, res, next) => {
   // const { authorization } = req.headers;
   const cookies = cookie.parse(req.headers.cookie);
   const auth = cookies.authorization;
-  console.log(req.cookies);
-  // const auth = req.cookies.authorization
 
-  console.log(auth);
+  // console.log(req.cookies);
+  // const auth = req.cookies.authorization
+  // console.log(auth);
 
   try {
     // const token = authorization.split(" ")[1];
     const token = auth;
     // console.log(token);
-    const decode = await jwt.verify(token, process.env.JWT_SECRET);
-    const { id, admin } = decode;
-    req.id = id;
-    req.admin = admin;
-    console.log(id);
-    next();
+    if (token == null || token == undefined) {
+      res.status(401).json({
+        status: 401,
+        message: "Token unavailable ",
+      });
+    } else {
+      const decode = await jwt.verify(token, process.env.JWT_SECRET);
+      const { id, admin } = decode;
+      req.id = id;
+      req.admin = admin;
+      console.log(id);
+      next();
+    }
   } catch {
-    next("Authentication failure!!! 404");
+    res.status(401).json({
+      status: 401,
+      message: "Token unavailable  ",
+    });
   }
 };
 module.exports = checkLogin;
