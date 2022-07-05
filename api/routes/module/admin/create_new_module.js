f76bb2e703f92a898927d302d9cf7faa506aa0f6
GopalUXD <gopal@uxd.co.jp>
2022-07-05 08:52:16 +0600

    ok

--- api/routes/module/admin/create_new_module.js
+++ api/routes/module/admin/create_new_module.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const { validate, ValidationError, Joi } = require("express-validation");
+const { validate, ValidationError, Joi } = require("express-validation");
 //Validations
-// const departmentValidations = require("../../../validations/department_validation");
+const moduleValidations = require("../../../validations/module_validation");
 // Middlewares
 const checkLogin = require("../../../middlewares/checkLogin.js");
 const checkAdmin = require("../../../middlewares/checkIsAdmin.js");
@@ -10,48 +10,48 @@ const checkAdmin = require("../../../middlewares/checkIsAdmin.js");
 const Module = require("../../../models/module_schema.js");
 
 router.post(
-  "/",
-  //   validate(departmentValidations.createOrUpdateDepartmentValidator),
-  checkLogin,
-  checkAdmin,
-  async (req, res) => {
-    try {
-      const cleckExistingTitle = await Module.findOne({
-        title: req.body.title,
-      });
-      if (cleckExistingTitle == null) {
-        const module = new Module({
-          title: req.body.title,
-          active_status: req.body.active_status,
-        });
-        const newModule = await module.save();
-        res.status(200).json({
-          status: 200,
-          message: "Module added successfully",
-          body: newModule,
-        });
-      } else {
-        res.status(409).json({
-          status: 409,
-          message: "Module already exist",
-          body: null,
-        });
-      }
-    } catch (err) {
-      res.status(400).json({
-        status: 400,
-        message: err.message,
-        body: null,
-      });
+    "/",
+    validate(moduleValidations.createOrUpdateModuleValidator),
+    checkLogin,
+    checkAdmin,
+    async(req, res) => {
+        try {
+            const cleckExistingTitle = await Module.findOne({
+                title: req.body.title,
+            });
+            if (cleckExistingTitle == null) {
+                const module = new Module({
+                    title: req.body.title,
+                    active_status: req.body.active_status,
+                });
+                const newModule = await module.save();
+                res.status(200).json({
+                    status: 200,
+                    message: "Module added successfully",
+                    body: newModule,
+                });
+            } else {
+                res.status(409).json({
+                    status: 409,
+                    message: "Module already exist",
+                    body: null,
+                });
+            }
+        } catch (err) {
+            res.status(400).json({
+                status: 400,
+                message: err.message,
+                body: null,
+            });
+        }
     }
-  }
 );
 
-// router.use((err, req, res, next) => {
-//   if (err instanceof ValidationError) {
-//     return res.status(err.statusCode).json(err);
-//   }
-//   return res.status(500).json(err);
-// });
+router.use((err, req, res, next) => {
+    if (err instanceof ValidationError) {
+        return res.status(err.statusCode).json(err);
+    }
+    return res.status(500).json(err);
+});
 
 module.exports = router;
