ca5c376f18ada7b1ca8194e27b4969aa0a4334df
EahsanOrnob <jamalmostafaeahsan@gmail.com>
2022-07-05 09:24:15 +0600

    v1

---
+++ api/routes/project/admin/create_new_project.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+// const { validate, ValidationError, Joi } = require("express-validation");
+//Validations
+// const departmentValidations = require("../../../validations/department_validation");
+// Middlewares
+const checkLogin = require("../../../middlewares/checkLogin.js");
+const checkAdmin = require("../../../middlewares/checkIsAdmin.js");
+// Models
+const Project = require("../../../models/project_schema.js");
+
+router.post(
+  "/",
+  //   validate(departmentValidations.createOrUpdateDepartmentValidator),
+  checkLogin,
+  checkAdmin,
+  async (req, res) => {
+    try {
+      const cleckExistingTitle = await Project.findOne({
+        title: req.body.title,
+      });
+      if (cleckExistingTitle == null) {
+        const project = new Module({
+          title: req.body.title,
+          active_status: req.body.active_status,
+        });
+        const newProject = await project.save();
+        res.status(200).json({
+          status: 200,
+          message: "Project added successfully",
+          body: newProject,
+        });
+      } else {
+        res.status(409).json({
+          status: 409,
+          message: "Project name already exist",
+          body: null,
+        });
+      }
+    } catch (err) {
+      res.status(400).json({
+        status: 400,
+        message: err.message,
+        body: null,
+      });
+    }
+  }
+);
+
+// router.use((err, req, res, next) => {
+//   if (err instanceof ValidationError) {
+//     return res.status(err.statusCode).json(err);
+//   }
+//   return res.status(500).json(err);
+// });
+
+module.exports = router;
