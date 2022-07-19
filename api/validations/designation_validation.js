a95e4ecbedb5efd27a7abfb68eed7781bd7b0171
EahsanOrnob <91877192+jamaluxd@users.noreply.github.com>
2022-07-19 10:35:07 +0600

    joi veli rank cng

--- api/validations/designation_validation.js
+++ api/validations/designation_validation.js
@@ -4,7 +4,7 @@ const designationValidations = {
   createOrUpdateDesignationValidator: {
     body: Joi.object({
       title: Joi.string().min(2).max(30).required(),
-      rank: Joi.boolean().required(),
+      rank: Joi.number().required(),
       active_status: Joi.boolean().required(),
       access_token: Joi.string().required(),
     }),
