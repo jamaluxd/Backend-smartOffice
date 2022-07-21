7f30a0758c8d1551f97664034891e05f226cc040
EahsanOrnob <91877192+jamaluxd@users.noreply.github.com>
2022-07-21 08:47:52 +0600

    add active status check on login

--- api/routes/employee/user/user_login.js
+++ api/routes/employee/user/user_login.js
@@ -1,19 +1,20 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
 dotenv.config();
 
 // Models
-const Employee = require("../../../models/employee_schema");
-const Department = require("../../../models/department_schema.js");
-const Designation = require("../../../models/designation_schema.js");
+const Employee = require('../../../models/employee_schema');
+const Department = require('../../../models/department_schema.js');
+const Designation = require('../../../models/designation_schema.js');
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const loginEmployee = await Employee.findOne({
       email: req.body.email,
+      active_status: true,
     });
     if (loginEmployee != null) {
       const isValidPassword = await bcrypt.compare(
@@ -29,47 +30,47 @@ router.post("/", async (req, res) => {
           },
           process.env.JWT_SECRET,
           {
-            expiresIn: "9h",
+            expiresIn: '9h',
           }
         );
         // set authorization cookie
-        res.cookie("authorization", token, {
+        res.cookie('authorization', token, {
           httpOnly: true,
           maxAge: 1000 * 60 * 60 * 8,
-          sameSite: "strict",
-          secure: process.env.NODE_ENV === "production",
-          path: "/",
+          sameSite: 'strict',
+          secure: process.env.NODE_ENV === 'production',
+          path: '/',
         });
 
         const findDepartmentTitleById = await Department.findById(
           loginEmployee.department_id,
-          "title"
+          'title'
         );
         loginEmployee.department = findDepartmentTitleById.title;
 
         const findDesignationTitleById = await Designation.findById(
           loginEmployee.designation_id,
-          "title"
+          'title'
         );
         loginEmployee.designation = findDesignationTitleById.title;
 
         res.status(200).json({
           status: 200,
           access_token: token,
-          message: "Login successfully",
+          message: 'Login successfully',
           body: loginEmployee,
         });
       } else {
         res.status(401).json({
           status: 401,
-          message: "Authentication Failed!!!",
+          message: 'Authentication Failed!!!',
           body: [],
         });
       }
     } else {
       res.status(401).json({
         status: 401,
-        message: "Authentication Failed!!!",
+        message: 'Authentication Failed!!!',
         body: [],
       });
     }
