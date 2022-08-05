c939d4685e72c9488da80f1a7f61956e890f0063
Rahiful Islam <rahiful@uxd.co.jp>
2022-08-05 15:33:30 +0600

    backend Schema update

--- api/models/evaluation_schema.js
+++ api/models/evaluation_schema.js
@@ -6,17 +6,141 @@ const EvaluationSchema = new mongoose.Schema({
         required: true,
     },
 
-    results: [{
-        question: {
-            type: Number,
-            required: true,
-        },
+    employee_autonomous: {
+        type: Number,
+        required: true,
+    },
 
-        answer: {
-            type: Number,
-            required: true,
-        },
-    }],
+    employee_humble: {
+        type: Number,
+        required: true,
+    },
+
+    employee_passionate: {
+        type: Number,
+        required: true,
+    },
+
+    employee_honest: {
+        type: Number,
+        required: true,
+    },
+
+    employee_relible: {
+        type: Number,
+        required: true,
+    },
+
+    employee_creative: {
+        type: Number,
+        required: true,
+    },
+
+    employee_confident: {
+        type: Number,
+        required: true,
+    },
+
+    employee_eager: {
+        type: Number,
+        required: true,
+    },
+
+    employee_positive: {
+        type: Number,
+        required: true,
+    },
+
+    //-----------------
+
+    developing_part: {
+        type: Number,
+        required: true,
+    },
+
+    spoke_up: {
+        type: Number,
+        required: true,
+    },
+
+    activity_vocal: {
+        type: Number,
+        required: true,
+    },
+
+    information_shared: {
+        type: Number,
+        required: true,
+    },
+
+    quick_share_issues: {
+        type: Number,
+        required: true,
+    },
+
+    helped_others: {
+        type: Number,
+        required: true,
+    },
+
+    skill_improved: {
+        type: Number,
+        required: true,
+    },
+
+    developing_on_schedule: {
+        type: Number,
+        required: true,
+    },
+
+    leadership_ability: {
+        type: Number,
+        required: true,
+    },
+
+    punctional_employee: {
+        type: Number,
+        required: true,
+    },
+
+    frequently_dose_off: {
+        type: Number,
+        required: true,
+    },
+
+    hearts_someone: {
+        type: Number,
+        required: true,
+    },
+
+    unnecessary_talks: {
+        type: Number,
+        required: true,
+    },
+
+    unnecessary_activities: {
+        type: Number,
+        required: true,
+    },
+
+    keep_equipment_good: {
+        type: Number,
+        required: true,
+    },
+
+
+
+    // results: [{
+    //     question: {
+    //         type: Number,
+    //         required: true,
+    //     },
+
+    //     answer: {
+    //         type: Number,
+    //         required: true,
+    //     },
+    // }],
 });
 
 module.exports.Evaluation = mongoose.model("Evaluation", EvaluationSchema);
